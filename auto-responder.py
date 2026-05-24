603a9054dfbaea40778410d7f5e1fc0cd9535ed4
bekhzodrakhimov-crypto <bekhzodrakhimov@gmail.com>
2026-05-24 13:33:24 +0500

    Create auto-responder.py

---
+++ auto-responder.py
@@ -0,0 +1,4 @@
+API_ID = 37110545       # ваш номер с my.telegram.org
+API_HASH = "8653918a5c9f2f34d2ccb681df85f648"  # ваш хэш
+ANTHROPIC_API_KEY = "sk-ant-..."  # ключ Claude
+YOUR_NAME = "Behzodjon"      # ваше имя
